60e1b89932c7bef18acbf4f06d14ea1d72eb2aab
Niall-Booth <104877087+Niall-Booth@users.noreply.github.com>
2026-02-17 18:44:33 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -29,20 +29,20 @@ function botMove(){
         const dashPos = values.indexOf("-");
 
         if(oCount === 2 && dashPos != -1){
-            cells[dashPos].find("h1").text("O");
+            cells[dashPos].find("span").text("O");
             return
         }
     }
 
     for(let pattern of winPatterns){
         let cells = pattern.map(([r,c]) => grid[r][c]);
-        let values = cells.map(cell => cell.find("h1").text());
+        let values = cells.map(cell => cell.find("span").text());
 
         const xCount = values.filter(value => value === "X").length;
         const dashPos = values.indexOf("-");
 
         if(xCount === 2 && dashPos != -1){
-            cells[dashPos].find("h1").text("O");
+            cells[dashPos].find("span").text("O");
             return
         }
     }
@@ -56,19 +56,20 @@ function botMove(){
     });
 
     const ranCellIndex = Math.floor(Math.random()*emptyCells.length);
-    emptyCells[ranCellIndex].find("h1").text("O");
+    emptyCells[ranCellIndex].find("span").text("O");
 }
 
 function checkForWin(piece){
     for(let pattern of winPatterns){
         let cells = pattern.map(([r,c]) => grid[r][c]);
-        let values = cells.map(cell => cell.find("h1").text());
+        let values = cells.map(cell => cell.find("span").text());
 
         const pieceCount = values.filter(value => value === piece).length;
 
         if(pieceCount === 3){
             cells.forEach(cell => {
-                cell.addClass("text-success")
+                if(piece === "X"){cell.addClass("text-success")}
+                else if(piece === "O"){cell.addClass("text-danger")}
             });
             
             if(piece === "X"){
@@ -111,7 +112,7 @@ for(let x = 1; x < 10; x++){
         if(playerWin || botWin){
             return
         }else{
-            const buttonHeading = $(`#${x} h1`);
+            const buttonHeading = $(`#${x} span`);
             if(buttonHeading.text() === "-"){
                 buttonHeading.text("X");
                 checkForWin("X");
@@ -146,9 +147,12 @@ $(".game-reset").on("click", function(){
     const allCells = $(".grid-button");
 
     allCells.each(function() {
-        $(this).find("h1").text("-");
+        $(this).find("span").text("-");
         if($(this).hasClass("text-success")){
             $(this).removeClass("text-success");
+        }
+        else if($(this).hasClass("text-danger")){
+            $(this).removeClass("text-danger")
         }
     });
 
